ed0420cec2a8783285b3b635fa30c34f7037fbdc
rosechanapa <s6404062630279@email.kmutnb.ac.th>
2023-10-12 00:38:14 +0700

    update

--- menu/menu.php
+++ menu/menu.php
@@ -39,7 +39,10 @@
             <?php
             echo $row["menu_name"];
             ?>
-
+            <form method="post" action="cart.php?action=add&menu_id=<?=$row["menu_id"]?>&menu_name=<?=$row["menu_name"]?>&price=<?=$row["price"]?>">
+				<input type="number" name="qty" value="1" min="1" max="9">
+				<input type="submit" value="ใส่ตะกร้า">	   
+			</form>
         </div>
     <?php endwhile; ?>
 
