fb467f35b71bcb5772bbee1a49b5f15ba9c6f780
Nattha45 <139251222+Nattha45@users.noreply.github.com>
2023-10-17 23:58:59 +0700

    index
    ทำหน้าindex อย่าลืมกดpuhsกันละ

--- chef/chef.php
+++ chef/chef.php
@@ -27,30 +27,26 @@
     <h1>รายการอาหารที่สั่ง</h1>
 
     <?php
-    // Check if a form was submitted
+         //ตรวจการส่งแบบฟอร์ม
     if (isset($_POST['order_id']) && isset($_POST['action'])) {
         $order_id = $_POST['order_id'];
         $action = $_POST['action'];
 
-        // Check user authorization and perform actions in the database
+        // ตรวจการอนุมัตuser&data
         if ($action == 'cancel') {
-            // Check user authorization here, e.g., based on user roles or session
             $authorized = true;
-
             if ($authorized) {
-                // Perform the cancel action in the database
-                // You need to write SQL query for this
+                //เขียนSQL query
                 echo "Order $order_id has been canceled.";
             } else {
                 echo "ไม่อนุญาตให้ยกเลิกคำสั่งซื้อนี้";
             }
         } elseif ($action == 'complete') {
-            // Check user authorization here, e.g., based on user roles or session
+            //ตรวจการให้สิทธิ์
             $authorized = true;
 
             if ($authorized) {
-                // Perform the complete action in the database
-                // You need to write SQL query for this
+                //ดำเนินการทั้งหมดในฐานข้อมูล
                 echo "Order $order_id has been completed.";
             } else {
                 echo "ไม่อนุญาตให้เสร็จสิ้นคำสั่งซื้อนี้";
@@ -58,7 +54,7 @@
         }
     }
 
-    // Query to retrieve order details with order_timestamp
+    // ดึงรายละเอียดคำสั่งซื้อด้วย order_timestamp
     $sql = "SELECT orders.order_id, menu.menu_name, orders.quantity, orders.detail, orders.order_timestamp FROM orders
             INNER JOIN menu ON orders.menu_id = menu.menu_id";
 
@@ -66,7 +62,7 @@
     $stmt->execute();
 
     if ($stmt->rowCount() > 0) {
-        echo "<form method='post' action=''>"; // Form to submit order actions
+        echo "<form method='post' action=''>"; //การสั่งซื้อ
 
         echo "<table>";
         echo "<tr><th>รหัสรายการ</th><th>ชื่อเมนู</th><th>จำนวน</th><th>รายละเอียด</th><th>เวลา</th><th>การดำเนินการ</th></tr>";
@@ -75,7 +71,7 @@
             $orderId = $row["order_id"];
             echo "<tr id='row_$orderId'><td>".htmlspecialchars($orderId)."</td><td>".htmlspecialchars($row["menu_name"])."</td><td>".htmlspecialchars($row["quantity"])."</td><td>".htmlspecialchars($row["detail"])."</td><td>".htmlspecialchars($row["order_timestamp"])."</td>";
 
-            // Add Cancel and Complete buttons for each order
+            //  Cancel&Completeของทุกๆorder
             echo "<td>
                 <input type='hidden' name='order_id' value='".$orderId."'>
                 <button type='submit' name='action' value='cancel' onclick='hideAndRemoveRow($orderId)'>Cancel</button>
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-session_start(); // เริ่มเซสชันหรือใช้เซสชันที่มีอยู่
+session_start();
 
 if (!isset($_SESSION['username'])) {
-    // ถ้าไม่มีเซสชันชื่อผู้ใช้งาน หมายความว่ายังไม่เข้าสู่ระบบ
-    header("Location: login.php"); // เด้งไปยังหน้า login.php
+    header("Location: login.php");
     exit();
 }
 ?>
@@ -13,12 +12,56 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Welcome</title>
+    <style>
+
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #F8F8FF;
+            text-align: center;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #E6E6FA;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            font-family: 'Your Desired Font', sans-serif;
+            font-size: 36px;
+            color: #A52A2A; 
+            text-align: center; 
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 10);
+
+        }
+
+        .menu-link {
+            display: block;
+            margin: 10px 0;
+            padding: 10px 20px;
+            background-color: #007BFF;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        .menu-link:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
-    <a href="./admin/admin.php">admin</a>
-    <a href="./chef/chef.php">chef</a>
-    <a href="./employee/employee.php">employee</a>
-    <a href="./menu/menu.php">menu</a>
+    <div class="container">
+        <h1>SCANSAVOR</h1>
+        <a class="menu-link" href="./admin/admin.php">Admin</a>
+        <a class="menu-link" href="./chef/chef.php">Chef</a>
+        <a class="menu-link" href="./employee/employee.php">Employee</a>
+        <a class="menu-link" href="./menu/menu.php">Menu</a>
+    </div>
 </body>
 </html>
