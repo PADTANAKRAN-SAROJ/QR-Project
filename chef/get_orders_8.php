7953633b40bba2e772fbbd6f986fa068c8a82f6a
Nattha45 <139251222+Nattha45@users.noreply.github.com>
2023-11-08 03:01:42 +0700

    phpbyu
    นอนล่ะ

--- chef/get_orders_8.php
+++ chef/get_orders_8.php
@@ -5,7 +5,7 @@ $category = "อื่นๆ"; // อื่นๆ
 $sql = "SELECT orders.order_id, menu.menu_name, menu.category, orders.quantity, orders.detail, orders.order_timestamp
     FROM orders
     JOIN menu ON orders.menu_id = menu.menu_id
-    WHERE orders.process = 'Cooking' AND menu.category = :category";
+    WHERE orders.process = 'Cooking' AND menu.category = :category ORDER BY `orders`.`order_id` ASC";
 
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':category', $category, PDO::PARAM_STR);
