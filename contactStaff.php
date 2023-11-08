d0005d505d79f0f51a695b89380823513e2b532b
PADTANAKRAN-SAROJ <s6404062630121@email.kmutnb.ac.th>
2023-11-08 07:39:25 +0700

    up csss

--- contactStaff.php
+++ contactStaff.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="./icon.png">
     <title>ติดต่อพนักงาน</title>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
 </head>
 <body>
     <h1>โปรดติดต่อพนักงาน</h1>
-    <h3>ขออภัยในความไม่สดวก</h3>
+    <h2>ขออภัยในความไม่สดวก</h2>
 </body>
 </html>
--- thankYou.php
+++ thankYou.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="./icon.png">
     <title>ขอบคุณ</title>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
 </head>
 <body>
     <h1>ขอบคุณที่อุดหนุน</h1>
-    <h3>ไว้มาทานอีกนะคร้าบบ</h3>
+    <h2>โอกาศหน้าเชิญใหม่</h2>
 </body>
 </html>
